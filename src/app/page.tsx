7b1788050d137f7c5f7bde9f741505f69e3604c6
ksg1103 <ksgwkd1103@naver.com>
2026-03-16 15:36:05 +0900

    백엔드 정보 받아와서 출력

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import Image from "next/image";
 
 export default function Home() {
+  
   return (
-    
+    <>
       <div>
         <h1>안녕하세요</h1>
       </div>
+      
+      </>
     
   );
 }
